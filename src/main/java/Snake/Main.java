96f7375d77f189360a9c34c2dea8276ffebc43a6
Sergio Trett <serge3ling@gmail.com>
2020-12-16 12:34:51 +0200

    Zero commit.

---
+++ src/main/java/Snake/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Snake;
+
+//import java.awt.EventQueue;
+//import javax.swing.JFrame;
+
+/**
+ *
+ * @author tret
+ */
+public class Main {
+    /*public Main() {
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }*/
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame.main(args);
+        /*EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {                
+                JFrame ex = new Main();
+                ex.setVisible(true);                
+            }
+        });*/
+    }
+}
